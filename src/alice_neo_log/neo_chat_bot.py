7c9ae701d05ae7d7d6c62eeb38bebd4cf99003ac
alice-code-kr <>
2025-11-18 22:42:32 +0900

    현재시간 가져오는 tools 추가함.

--- src/alice_neo_log/neo_chat_bot.py
+++ src/alice_neo_log/neo_chat_bot.py
@@ -5,24 +5,22 @@ from langchain_core.messages import ChatMessage
 from langchain.tools import tool
 from datetime import datetime
 from langchain.messages import AIMessage
+from langchain.messages import ToolMessage
 
+
+@tool
+def get_now() -> str:
+   """이거는 한국 현재시간을 알려주는 함수야. 한국 현재 시간을 물어보면 이 함수를 실행해."""
+   now = datetime.now().strftime("%Y년 %m월 %d일 %H시 %M분 %S.%f초")
+   return now
 class NeoChatBot():
    def __init__(self):
       self.llm = ChatOllama(
-         model="gemma3:12b",
+         model="gpt-oss:latest",
          temperature=0,
          validate_model_on_init=True,
-         tools=self.get_now,
          base_url="http://192.168.219.109:11434"
-      )
-
-   @tool
-   def get_now(self) -> str:
-      """이거는 한국 현재시간을 알려주는 함수야. 한국 현재 시간을 물어보면 이 함수를 실행해."""
-      now = datetime.now()
-      print("=================================================================================")
-      return now
-
+      ).bind_tools([get_now])
 
    def ask(self,message):
       messages = [
@@ -32,7 +30,19 @@ class NeoChatBot():
       ai_msg = self.llm.invoke(messages)
       print(ai_msg)
       if isinstance(ai_msg, AIMessage) and ai_msg.tool_calls:
-         print(ai_msg.tool_calls)
+         for k in ai_msg.tool_calls:
+            if k["name"] == 'get_now': 
+               tool = ToolMessage( 
+                 content=get_now.invoke({}),
+                 tool_call_id= k["id"] 
+               )
+               messages.append(tool)
+               print(self.llm.invoke(messages))
+
+         
+
+            print(ai_msg.tool_calls)
+         
    
 
 
